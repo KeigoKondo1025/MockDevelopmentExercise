93e1f3e25b3c176ae289c96a733857825ef151d9
KeigoKondo1025 <107822586+KeigoKondo1025@users.noreply.github.com>
2022-06-23 16:07:45 +0900

    Add files via upload

--- MockDevelopmentExercise/WEB-INF/src/servlet/ListServlet.java
+++ MockDevelopmentExercise/WEB-INF/src/servlet/ListServlet.java
@@ -40,4 +40,33 @@ public class ListServlet extends HttpServlet {
 			}
 		}
 	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String error = "";
+		String cmd = "";
+		String message = "";// 入力データが間違っていた場合ログイン画面に表示する
+
+		ItemDAO itemDaoObj = new ItemDAO();
+
+
+		try {
+			ArrayList<Item> itemList = itemDaoObj.selectAll();
+			request.setAttribute("item_list", itemList);
+
+		} catch (IllegalStateException e) {
+			error = "DB接続エラーの為、ログインは出来ません。";
+			cmd = "logout";
+		} finally {
+
+			if (!error.equals("")) {
+//				エラーがあればerror.jspへ
+				request.setAttribute("error", error);
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+//			エラーが無ければトップページへ
+			} else {
+				request.getRequestDispatcher("/view/index.jsp").forward(request, response);
+			}
+		}
+	}
 }
