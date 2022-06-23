c574587813a35ee0de92251658655b48c2de0fec
satsuki-ito07 <107829398+satsuki-ito07@users.noreply.github.com>
2022-06-23 13:31:24 +0900

    Add files via upload

--- MockDevelopmentExercise/WEB-INF/src/servlet/UpdateUserInfoServlet.java
+++ MockDevelopmentExercise/WEB-INF/src/servlet/UpdateUserInfoServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.*;
+import java.util.*;
+import java.sql.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -10,7 +12,8 @@ public class UpdateUserInfoServlet extends HttpServlet {
 		bean.User user = new bean.User();
 		String error = "";
 		String cmd = "";
-		String postalCode = "";
+		int count = 0;
+		dao.UserDAO userDao = new dao.UserDAO();
 		try {
 			request.setCharacterEncoding("UTF-8");
 			//各setterメソッドを利用し、ユーザー情報を設定
@@ -18,10 +21,27 @@ public class UpdateUserInfoServlet extends HttpServlet {
 			user.setFamilyName(request.getParameter("familyName"));
 			user.setFirstName(request.getParameter("firstName"));
 			user.setGender(Integer.parseInt(request.getParameter("gender")));
-			postalCode
+			user.setPostalCode(request.getParameter("postalCode"));
+			user.setPrefectureId(Integer.parseInt(request.getParameter("prefectureId")));
+			user.setAddress1(request.getParameter("address1"));
+			Date birthday = Date.valueOf(request.getParameter("birthday"));
+			user.setBirthday(birthday);
+			user.setPhoneNumber(request.getParameter("phoneNumber"));
+			count = userDao.update(user);
+
 		} catch (IllegalStateException e) {
 			error = "DB接続エラーのため、ユーザー情報の変更は行えませんでした。";
 			cmd = "error";
+		} finally {
+			if (cmd.equals("")) {
+				//UserInfoServletに遷移する
+				request.getRequestDispatcher("/view/UserInfo").forward(request, response);
+			} else {
+				request.setAttribute("error", error);
+				request.setAttribute("cmd", cmd);
+				//error.jspに遷移する
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+			}
 		}
 	}
 }
