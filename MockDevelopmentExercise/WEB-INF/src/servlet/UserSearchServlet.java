a8b8dba7b45a808bc26b6c8c4a5f0b4678f8c136
tokutoku7 <107829507+tokutoku7@users.noreply.github.com>
2022-06-27 13:39:11 +0900

    Add files via upload

---
+++ MockDevelopmentExercise/WEB-INF/src/servlet/UserSearchServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import bean.*;
+import dao.*;
+
+public class UserSearchServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+
+		String error = "";
+		String cmd = "";
+
+		try {
+			UserDAO userDao = new UserDAO();
+
+			String userName = (String)request.getParameter("userName");
+
+			ArrayList<Integer> userIdList = userDao.searchByUserName(userName);
+			request.setAttribute("userIdList", userIdList);
+
+		} catch (IllegalStateException e) {
+			error = "DB接続エラーの為、検索出来ません。";
+		} finally {
+
+			if (!error.equals("")) {
+//				エラーがあればerror.jspへ
+				request.setAttribute("error", error);
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+			}else {
+//				エラーが無ければトップページへ
+				request.getRequestDispatcher("/view/userList.jsp").forward(request, response);
+			}
+
+		}
+
+	}
+
+}
