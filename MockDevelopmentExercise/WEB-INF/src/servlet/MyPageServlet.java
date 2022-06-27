e19bae220475ba82b698221c4329d3190290b426
tokutoku7 <107829507+tokutoku7@users.noreply.github.com>
2022-06-27 17:31:57 +0900

    Update MyPageServlet.java

--- MockDevelopmentExercise/WEB-INF/src/servlet/MyPageServlet.java
+++ MockDevelopmentExercise/WEB-INF/src/servlet/MyPageServlet.java
@@ -52,4 +52,41 @@ public class MyPageServlet extends HttpServlet {
 
 		}
 	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String error = "";
+
+		request.setCharacterEncoding("UTF-8");
+
+		try {
+
+			HttpSession session = request.getSession();
+			User user = (User) session.getAttribute("user");
+
+			if (user == null) {//セッション切れ判定
+				request.setAttribute("error", "MyPageServletでのセッション切れ");
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+				return;
+			}
+
+			ItemDAO itemDaoObj = new ItemDAO();
+			String sellerId = "" + user.getUserId();
+			ArrayList<Item> itemList = itemDaoObj.select(sellerId, "", "","");
+
+			request.setAttribute("itemList", itemList);
+			request.getRequestDispatcher("/view/myPage.jsp").forward(request, response);
+
+		} catch (IllegalStateException e) {
+			error = "DB接続エラー";
+			request.setAttribute("error", error);
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			error = "予期せぬエラーが発生しました。<br>" + e;
+			request.setAttribute("error", error);
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+
+		}
+	}
 }
